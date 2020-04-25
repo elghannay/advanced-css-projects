f853c1e855a4b447f1a94f2977dd442005216d9a
elghannay <mohamedelghannay@gmail.com>
2020-04-25 04:41:49 +0000

    adding the mobile navigation

--- uHost/modal.js
+++ uHost/modal.js
@@ -3,15 +3,31 @@ const backdrop = document.querySelector('.backdrop');
 const modal = document.querySelector('.modal');
 const modal_no = document.querySelector('.modal__action--negative');
 const modal_yes = document.querySelector('.modal__action');
+const mobile_nav = document.querySelector('.mobile-nav');
+const toggle_button = document.querySelector('.toggle-button');
+
+function closeModal() {
+    backdrop.classList.remove('open');
+    if (modal)
+        modal.classList.remove('open');
+}
 
 btn.forEach(element => {
     element.addEventListener('click', () => {
-        backdrop.style.display = 'block';
-        modal.style.display = 'block';
+        backdrop.classList.add('open');
+        modal.classList.add('open');
     })
 });
+if (modal_no) modal_no.addEventListener('click', closeModal);
 
-modal_no.addEventListener('click', () => {
-    backdrop.style.display = 'none';
-    modal.style.display = 'none';
+toggle_button.addEventListener('click', () => {
+    mobile_nav.classList.add('open');
+    backdrop.classList.add('open');
+})
+
+backdrop.addEventListener('click', () => {
+    mobile_nav.classList.remove('open');
+    backdrop.classList.remove('open');
+    if (modal)
+        modal.classList.remove('open');
 })
